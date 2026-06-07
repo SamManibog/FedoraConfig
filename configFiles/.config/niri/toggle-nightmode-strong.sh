f34fe7c1ea8f10a12f40de54c67f3656f9f34c9b
Sam Manibog <sfmanibog@outlook.com>
2026-06-06 17:59:31 -0700

    improved nightmode keybinds

---
+++ configFiles/.config/niri/toggle-nightmode-strong.sh
@@ -0,0 +1,1 @@
+pkill -f gammastep-weak ; pkill -f gammastep-strong || exec -a gammastep-strong gammastep -O 2000
---
+++ configFiles/.config/niri/toggle-nightmode-weak.sh
@@ -0,0 +1,1 @@
+pkill -f gammastep-strong ; pkill -f gammastep-weak || exec -a gammastep-weak gammastep -O 3500
