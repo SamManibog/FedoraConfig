f2d0654243e93ba627d74ed86e03628ce522531b
Sam Manibog <sfmanibog@outlook.com>
2026-07-10 18:32:09 -1000

    added assumeyes flag to waybar update all

--- userFiles/static/.local/bin/waybar-update-all.py
+++ userFiles/static/.local/bin/waybar-update-all.py
@@ -13,7 +13,7 @@ signal.signal(signal.SIGTERM, gracefulExit)
 signal.signal(signal.SIGINT, gracefulExit)
 
 try:
-    cmd = "sudo dnf update --refresh ; flatpak update"
+    cmd = "sudo dnf -y update --refresh ; flatpak update -y"
     print(cmd)
     subprocess.run(cmd, shell=True)
 finally:
